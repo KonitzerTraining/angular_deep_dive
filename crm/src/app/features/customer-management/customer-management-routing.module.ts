931c1c4551644e77c6cd4af532db5725dcd2772e
Marcus Konitzer <mk@konitzer-training.de>
2022-08-22 10:12:45 +0000

    Initial

--- crm/src/app/features/customer-management/customer-management-routing.module.ts
+++ crm/src/app/features/customer-management/customer-management-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomerManagementIndexComponent} from "./customer-management-index/customer-management-index.component";
+import {CustomerListComponent} from "./views/customer-list/customer-list.component";
+import {CustomerEditComponent} from "./views/customer-edit/customer-edit.component";
+import {CustomerDetailComponent} from "./views/customer-detail/customer-detail.component";
+import {CustomerNewComponent} from "./views/customer-new/customer-new.component";
 
 const routes: Routes = [
   {
     path: 'customer-management',
-    component: CustomerManagementIndexComponent
+    component: CustomerManagementIndexComponent,
+    children: [
+      {
+        path: '',
+        component: CustomerListComponent
+      },
+      {
+        path: 'edit/:id',
+        component: CustomerEditComponent
+      },
+      {
+        path: 'detail/:id',
+        component: CustomerDetailComponent
+      },
+      {
+        path: 'new',
+        component: CustomerNewComponent
+      }
+    ]
   }
 ];
 
--- crm/src/app/features/customer-management/customer-management.module.ts
+++ crm/src/app/features/customer-management/customer-management.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerManagementRoutingModule } from './customer-management-routing.module';
 import { CustomerManagementIndexComponent } from './customer-management-index/customer-management-index.component';
+import { CustomerListComponent } from './views/customer-list/customer-list.component';
+import { CustomerNewComponent } from './views/customer-new/customer-new.component';
+import { CustomerEditComponent } from './views/customer-edit/customer-edit.component';
+import { CustomerDetailComponent } from './views/customer-detail/customer-detail.component';
 
 
 @NgModule({
   declarations: [
-    CustomerManagementIndexComponent
+    CustomerManagementIndexComponent,
+    CustomerListComponent,
+    CustomerNewComponent,
+    CustomerEditComponent,
+    CustomerDetailComponent
   ],
   imports: [
     CommonModule,
