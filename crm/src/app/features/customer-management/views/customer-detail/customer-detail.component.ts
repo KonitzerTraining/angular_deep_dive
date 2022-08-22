e498b7153b55ca91f844724050759f47b68f6c5d
Marcus Konitzer <mk@konitzer-training.de>
2022-08-22 13:17:51 +0000

    Initial

--- crm/src/app/features/customer-management/views/customer-detail/customer-detail.component.ts
+++ crm/src/app/features/customer-management/views/customer-detail/customer-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {switchMap} from "rxjs/operators";
+import {CustomerService} from "../../services/customer.service";
+import {Customer} from "../../model/customer";
 
 @Component({
   selector: 'app-customer-detail',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-detail.component.scss']
 })
 export class CustomerDetailComponent implements OnInit {
+  public customer !: Customer;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private customerService: CustomerService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap((params) => {
+          return this.customerService.getOne(+params['id'])
+        })
+      )
+      .subscribe((customer) => {
+        this.customer = customer;
+      })
   }
 
 }
